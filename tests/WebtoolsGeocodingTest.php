4c6694dcc83b224a481c0c9966524e40c83f6aa2
Pieter Frenssen <pieter@frenssen.be>
2020-09-15 17:58:56 +0300

    OPENEUROPA-2986: Adhere to coding standards.

--- tests/WebtoolsGeocodingTest.php
+++ tests/WebtoolsGeocodingTest.php
@@ -1,8 +1,8 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
-/*
+/**
  * This file is part of the Geocoder package.
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
