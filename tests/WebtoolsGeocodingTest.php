d4b27192c3d1d6d6df7b6a0f8165ed1abde8758c
Claudiu Cristea <clau.cristea@gmail.com>
2022-11-16 21:12:58 +0200

    ISAICP-7452: Adapt test coverage.

--- tests/WebtoolsGeocodingTest.php
+++ tests/WebtoolsGeocodingTest.php
@@ -56,59 +56,55 @@ final class WebtoolsGeocodingTest extends BaseTestCase
         $results = $provider->geocodeQuery(GeocodeQuery::create('10 avenue Gambetta, Paris, France'));
 
         static::assertInstanceOf('Geocoder\Model\AddressCollection', $results);
-        static::assertCount(5, $results);
+        static::assertCount(1, $results);
 
         /** @var Location $result */
         $result = $results->first();
         static::assertInstanceOf('\Geocoder\Model\Address', $result);
-        static::assertEquals(48.865195518466, $result->getCoordinates()->getLatitude(), '', 0.0001);
-        static::assertEquals(2.3987030416067, $result->getCoordinates()->getLongitude(), '', 0.0001);
+        static::assertEquals(48.8631927, $result->getCoordinates()->getLatitude(), '', 0.0001);
+        static::assertEquals(2.3890894, $result->getCoordinates()->getLongitude(), '', 0.0001);
         static::assertEquals(10, $result->getStreetNumber());
-        static::assertEquals('10 Avenue Gambetta', $result->getStreetName());
+        static::assertEquals('Avenue Gambetta', $result->getStreetName());
         static::assertEquals(75020, $result->getPostalCode());
         static::assertEquals('Paris', $result->getLocality());
-        static::assertEquals('Île-de-France', $result->getAdminLevels()->get(1)->getName());
 
         // The following data is not returned yet in the current implementation
         // of the Webtools Geocoding API.
+        true || static::assertEquals('Île-de-France', $result->getAdminLevels()->get(1)->getName());
         true || static::assertCount(2, $result->getAdminLevels());
         true || static::assertEquals('Paris', $result->getAdminLevels()->get(2)->getName());
-        true || static::assertEquals('FRA', $result->getCountry()->getCode());
     }
 
     /**
      * @dataProvider geocodeWithCityProvider
      */
-    public function testGeocodeWithCity(string $region, string $sub_region, string $country_code, float $longitude, float $latitude): void
+    public function testGeocodeWithCity(int $delta, float $longitude, float $latitude): void
     {
         $provider = new WebtoolsGeocoding($this->getHttpClient());
         $results = $provider->geocodeQuery(GeocodeQuery::create('Hannover'));
 
         static::assertInstanceOf('Geocoder\Model\AddressCollection', $results);
-        static::assertCount(5, $results);
+        static::assertCount(10, $results);
 
         $result_found = false;
         /** @var Location $result */
-        foreach ($results as $result) {
-            if ($result->getAdminLevels()->get(1)->getName() === $region) {
+        foreach ($results as $index => $result) {
+            if ($index === $delta) {
                 $result_found = true;
 
                 static::assertInstanceOf('\Geocoder\Model\Address', $result);
-                static::assertEquals($longitude, $result->getCoordinates()->getLongitude(), '', 0.0001);
+                static::assertEquals($longitude, $result->getCoordinates()->getLongitude()/*, '', 0.0001*/);
                 static::assertEquals($latitude, $result->getCoordinates()->getLatitude(), '', 0.0001);
                 static::assertNull($result->getStreetNumber());
                 static::assertNull($result->getStreetName());
                 static::assertNull($result->getPostalCode());
-                static::assertEquals('Hannover', $result->getLocality());
-                static::assertCount(2, $result->getAdminLevels());
-                static::assertEquals($region, $result->getAdminLevels()->get(1)->getName());
-                static::assertEquals($sub_region, $result->getAdminLevels()->get(2)->getName());
-                static::assertEquals($country_code, $result->getCountry()->getCode());
 
                 // The following data is not returned yet in the current implementation
                 // of the Webtools Geocoding API.
                 true || static::assertEquals('Germany', $result->getCountry()->getName());
-
+                true || static::assertCount(2, $result->getAdminLevels());
+                true || static::assertEquals($region, $result->getAdminLevels()->get(1)->getName());
+                true || static::assertEquals($sub_region, $result->getAdminLevels()->get(2)->getName());
                 break;
             }
         }
@@ -127,25 +123,19 @@ final class WebtoolsGeocodingTest extends BaseTestCase
     {
         return [
             [
-                'region' => 'Niedersachsen',
-                'sub_region' => 'Region Hannover',
-                'country_code' => 'DEU',
-                'longitude' => 9.738150,
-                'latitude' => 52.37227,
+                'delta' => 0,
+                'longitude' => 9.738150000000076,
+                'latitude' => 52.37227000000007,
             ],
             [
-                'region' => 'Maryland',
-                'sub_region' => 'Frederick',
-                'country_code' => 'USA',
-                'longitude' => -77.44026,
-                'latitude' => 39.39177,
+                'delta' => 2,
+                'longitude' => -101.42142999999999,
+                'latitude' => 47.111290000000054,
             ],
             [
-                'region' => 'North Dakota',
-                'sub_region' => 'Oliver County',
-                'country_code' => 'USA',
-                'longitude' => -101.42143,
-                'latitude' => 47.11129,
+                'delta' => 4,
+                'longitude' => 9.740000000000066,
+                'latitude' => 52.44889000000006,
             ],
         ];
     }
